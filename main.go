c95888deb0fe7c4d8de56765ad64433a82899e3d
Sergey Klimov <myxxze@gmail.com>
2026-01-17 23:35:27 +0300

    - Added test md. - Added file opening.

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+)
+
+var (
+	file = flag.String("file", "", "path to file for indexation")
 )
 
 func main() {
-	fmt.Printf("Indexator-2000\n")
+	fmt.Printf("Индексатор-2000 загружается.\n")
+
+	flag.Parse()
+
+	filename := *file
+
+	if filename == "" {
+		fmt.Printf("Ты чё, еблан? Пошёл нахуй с таким файлом.\n") // todo: добавить сюда генерацию файла с медведем с хуем
+		return
+	}
+
+	_, err := os.OpenFile(filename, os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Printf("error os.OpenFile: %v\n", err)
+	}
 }
---
+++ mds/simple.md
@@ -0,0 +1,7 @@
+# Heading
+
+## Subheading
+
+### Sub-subheading
+
+## Subheading 2
